9dcf1d9b4f25ea43d6ac19b3fe50805f54397236
AnhDung Lam <anhdunglamvn@gmail.com>
2019-05-22 11:59:37 +0200

    delete comment code

--- RNN Stock.py
+++ RNN Stock.py
@@ -19,6 +19,7 @@ from keras.layers import Dense, LSTM
 varTimeStamp = 60
 training_Set_percent = 0.8
 
+##data = pd.read_csv("NSE-TATAGLOBAL11.csv")
 data = pd.read_csv("NSE-TATAGLOBAL.csv")
 
 dataset= pd.DataFrame(index=range(0,len(data)), columns=['Date','Close'])
@@ -84,92 +85,3 @@ import math
 from sklearn.metrics import mean_squared_error
 rmse = math.sqrt(mean_squared_error(valid['Close'], valid['Pred']))
 print("MSE: %.1f" %rmse)
-
-"""
-
-#######ANN
-X_ann = data.iloc[:, 4:-1]
-y_ann = data.iloc[:, -1]
-split = int(len(data)*0.8)
-X_train_ann, X_test_ann, y_train_ann, y_test_ann =X_ann[:split], X_ann[split:], y_ann[:split], y_ann[split:]
-
-##Scale
-from sklearn.preprocessing import StandardScaler
-sc_ann = StandardScaler()
-X_train_ann = sc_ann.fit_transform(X_train_ann)
-X_test_ann = sc_ann.transform(X_test_ann)
-
-
-ann = Sequential()
-ann.add(Dense(units=50, kernel_initializer='uniform', activation='relu', input_dim= X_test_ann.shape[1]))
-ann.add(Dense(units=50, kernel_initializer='uniform', activation='relu'))
-ann.add(Dense(units=50, kernel_initializer='uniform', activation='sigmoid'))
-ann.compile(optimizer='adam', loss="binary_crossentropy", metrics=['accuracy'])
-ann.fit(X_train_ann,y_train_ann, epochs=10, batch_size=10, verbose=2)
-
-
-"""
-
-"""
-#read file
-df = pd.read_csv('NSE-TATAGLOBAL11.csv')
-df.head()
-
-data = df.sort_index(ascending = True, axis=0)
-new_data = pd.DataFrame(index=range(0,len(df)), columns=['Date','Close'])
-for i in range(0, len(new_data)):
-    new_data['Date'][i] = data['Date'][i]
-    new_data['Close'][i] = data['Close'][i]
-    
-new_data.index = new_data.Date
-new_data.drop('Date', axis=1, inplace =True)
-
-##Creating dataset train and test
-dataset = new_data.values
-X = pd.DataFrame(dataset)
-limit_train =int(len(dataset)*training_Set_percent)-1
-train = dataset[0:limit_train, :]
-valid = dataset[limit_train:, :]
-
-#Scale dataset train
-scale = MinMaxScaler(feature_range=(0,1))
-scaled_data = scale.fit_transform(dataset)
-
-x_train, y_train =[],[]
-
-for i in range(varTimeStamp, len(train)):
-    x_train.append(scaled_data[i-varTimeStamp:i,0])
-    y_train.append(scaled_data[i, 0])
-x_train, y_train = np.array(x_train), np.array(y_train)
-
-x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
-
-####model rnn
-rnn = Sequential()
-rnn.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1) ))
-rnn.add(LSTM(units=50))
-rnn.add(Dense(1))
-
-rnn.compile(optimizer='adam', loss='mean_squared_error')
-rnn.fit(x_train,y_train,epochs=10, batch_size=1, verbose=2)
-
-####Predict model
-inputs = new_data[len(new_data)-len(valid)-varTimeStamp:].values
-inputs = inputs.reshape(-1,1)
-inputs = scale.transform(inputs)
-
-x_test = []
-for i in range(varTimeStamp, inputs.shape[0]):
-    x_test.append(inputs[i-varTimeStamp:i,0])
-x_test = np.array(x_test)
-
-x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
-closing_price = rnn.predict(x_test)
-closing_price = scale.inverse_transform(closing_price)
-
-train = new_data[:limit_train]
-valid = new_data[limit_train:]
-valid['Pred'] = closing_price
-plt.plot(train['Close'])
-plt.plot(valid[['Close','Pred']])
-"""
